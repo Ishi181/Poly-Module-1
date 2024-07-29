ee3f69c27176979887b495970cb06856922aeeef
Ishi181 <139246938+Ishi181@users.noreply.github.com>
2024-07-29 22:01:24 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,21 +1,7 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through node <script>.
-//
-// You can also run a script with npx hardhat run <script>. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
-async function main() {
+module.exports = buildModule("deploy", (m) => {
+  const dep = m.contract("MyNFT", []);
 
-  const token = await hre.ethers.deployContract("MyNFT");
-
-  console.log("Token address:", await token.getAddress(),);
-}
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+  return { dep };
 });
